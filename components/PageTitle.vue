ad069d274c7118c0ef622d376bfb92f6009d9db4
Guilherme Moura <mouraggui@gmail.com>
2022-11-21 18:49:18 -0300

    feat: PageTitle component

---
+++ components/PageTitle.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1 class="text-6xl leading-2 text-blue-400 font-light mb-12 text-center">
+		{{ title }}
+	</h1>
+</template>
+
+<script>
+export default {
+	name: 'PageTitle',
+
+	props: {
+		title: String
+	}
+};
+</script>
