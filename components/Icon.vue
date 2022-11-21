0ddf36ae2bedc24324504b995470d13d35a03a4b
Guilherme Moura <mouraggui@gmail.com>
2022-11-21 18:46:28 -0300

    feat: Icon component

---
+++ components/Icon.vue
@@ -0,0 +1,39 @@
+<template>
+	<div
+		v-if="name"
+		@click="$emit('click')"
+		:class="classColor"
+		:style="{
+			width: size,
+			height: size,
+			mask: maskValue,
+			webkitMask: maskValue
+		}"
+	/>
+</template>
+
+<script>
+export default {
+	name: 'Icon',
+
+	props: {
+		name: String,
+
+		size: {
+			default: '20px',
+			typr: String
+		},
+
+		classColor: {
+			default: 'bg-black',
+			typr: String
+		}
+	},
+
+	computed: {
+		maskValue() {
+			return `url('${require(`@/assets/icons/${this.name}`)}') center center / contain no-repeat`;
+		}
+	}
+};
+</script>
